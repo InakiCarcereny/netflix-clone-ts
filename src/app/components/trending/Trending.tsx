2c05dd8e38c7f4de8d222e7fd8d45e71cf772708
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-06 21:15:08 -0300

    Refactor Trending component to use Carousel for better layout; enhances user experience with improved film display and styling

--- src/app/components/trending/Trending.tsx
+++ src/app/components/trending/Trending.tsx
@@ -1,25 +1,41 @@
 /* eslint-disable @next/next/no-img-element */
 import { Tittle } from '@/components/ui/tittle';
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
 import films from '@/mocks/data.json';
 
 export function Trending() {
   return (
-    <section className='max-w-[1524px] w-full h-full mx-auto mt-8'>
+    <section className='max-w-[1524px] w-full h-full mx-auto mt-8 flex flex-col gap-4'>
       <Tittle label='Trending Now' />
 
-      <ul className='flex items-center gap-4'>
-        {films.map((film) => (
-          <div key={film.id} className='flex place-items-end'>
-            <span>{film.id}</span>
-            <img
-              className='rounded-lg h-[200px] w-[100px]'
-              src={film.poster}
-              alt={film.title}
-            />
-          </div>
-        ))}
-      </ul>
+      <Carousel className='w-full max-w-full'>
+        <CarouselContent className=''>
+          {films.map((film) => (
+            <CarouselItem key={film.id} className='basis-1/7'>
+              <button className='relative'>
+                <span className='text-black text-8xl absolute top-36 -left-5'>
+                  {film.id}
+                </span>
+                <img
+                  className='rounded-lg h-[250px] w-[175px] object-cover'
+                  src={film.poster}
+                  alt={film.title}
+                />
+              </button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </section>
   );
 }
