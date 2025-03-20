4df4433779e765b692f21d9597898006f0c71d34
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-20 14:06:36 -0300

    Refactor HomeLayout and Home page to streamline code, remove unnecessary imports, and improve visual presentation with new styles

--- src/app/(routes)/home/layout.tsx
+++ src/app/(routes)/home/layout.tsx
@@ -1,19 +1,11 @@
-import { Info, Netflix, Play } from '@/icons';
+import { Netflix } from '@/icons';
 import { ReactNode } from 'react';
 import { Nav, User } from '@/components';
-import { fontClass } from '@/utils';
-
-import firstFilm from '@/mocks/data.json';
-
-const BUTTONS = [
-  { label: 'Reproduce', icon: <Play /> },
-  { label: 'More Info', icon: <Info /> },
-];
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <header className='w-full h-[100px] bg-black/10'>
+      <header className='w-full h-[100px] from-black/30 via-black/20 to-zinc-400/10 bg-gradient-to-b'>
         <div className='max-w-[1720px] mx-auto w-full h-full flex items-center justify-between'>
           <div className='flex items-center gap-8'>
             <h1>
@@ -26,37 +18,6 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
           <User />
         </div>
       </header>
-
-      <section className='max-w-[1720px] mx-auto w-full h-[800px] flex flex-col relative'>
-        <div className='absolute top-90 flex flex-col gap-4 max-w-[500px] w-full'>
-          <h2
-            className={`text-8xl text-white font-semibold ${fontClass(firstFilm[4].id)}`}
-          >
-            {firstFilm[4].title}
-          </h2>
-          <p className='text-white/90 font-bold text-xl'>
-            {firstFilm[4].description}
-          </p>
-
-          <div className='flex items-center gap-4'>
-            {BUTTONS.map((button) => (
-              <button
-                key={button.label}
-                className='flex items-center gap-4 bg-white text-black font-semibold text-xl px-12 py-3 rounded-[4px]'
-              >
-                {button.icon}
-
-                {button.label}
-              </button>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <img
-        src={firstFilm[4].poster[1]}
-        className='absolute top-0 left-0 w-full h-[900px] object-cover -z-10'
-      />
 
       <main className='max-w-[1720px] mx-auto w-full h-full'>{children}</main>
     </>
--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,3 +1,40 @@
+import { fontClass } from '@/utils';
+import firstFilm from '@/mocks/data.json';
+import { BUTTONS } from '@/lib';
+
 export default function Home() {
-  return <div>hola</div>;
+  return (
+    <>
+      <section className='max-w-[1720px] mx-auto w-full h-[800px] flex flex-col relative'>
+        <div className='absolute top-90 flex flex-col gap-4 max-w-[500px] w-full'>
+          <h2
+            className={`text-8xl text-white font-semibold ${fontClass(firstFilm[4].id)}`}
+          >
+            {firstFilm[4].title}
+          </h2>
+          <p className='text-white/90 font-bold text-xl'>
+            {firstFilm[4].description}
+          </p>
+
+          <div className='flex items-center gap-4'>
+            {BUTTONS.map((button) => (
+              <button
+                key={button.label}
+                className={`flex items-center gap-4 font-bold text-xl px-10 py-1 rounded-[4px] w-full ${button.className}`}
+              >
+                {button.icon}
+
+                {button.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <img
+        src={firstFilm[4].poster[1]}
+        className='absolute top-0 left-0 w-full h-[900px] object-cover -z-10'
+      />
+    </>
+  );
 }
